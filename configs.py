565674889edf1f81d750ffe55690d7dba3c40a0f
Mattymat123 <cfhalberg@gmail.com>
2025-02-10 22:14:13 +0100

    latest

--- configs.py
+++ configs.py
@@ -1,11 +1,10 @@
-TOKEN_Qwen = "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"
-Token_Gemini = "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"
-TOKEN_Phi = "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"
-
+import secrets
+import streamlit as st
+Gemini = st.secrets['Gemini']
 MODEL_CONFIG = {
     "coding": {
         "model_name": "qwen/qwen-2.5-coder-32b-instruct",
-        "api_key": TOKEN_Qwen,
+        "api_key": Gemini,
         "template": (
             "You are an expert programmer and debugging assistant. Provide clear, concise "
             "coding solutions and explanations. Answer in the language of the user's input (Danish or English).\n\n"
@@ -14,7 +13,7 @@ MODEL_CONFIG = {
     },
     "ifeval": {
         "model_name": "meta-llama/llama-3.1-70b-instruct",
-        "api_key": Token_Gemini,
+        "api_key": Gemini,
         "template": (
             "You are a helpful assistant skilled in reasoning and problem solving. Provide thoughtful, "
             "detailed explanations for instructive or evaluative queries. Answer in the language of the user's input (Danish not swedish or English) Keep the responses short and concise.\n\n"
@@ -23,7 +22,7 @@ MODEL_CONFIG = {
     },
     "mmlu": {
         "model_name": "microsoft/phi-4",  # Replace with the actual Phi model name
-        "api_key": TOKEN_Phi,
+        "api_key": Gemini,
         "template": (
             "You are an academic expert with broad knowledge. Provide well-reasoned, accurate answers "
             "to general knowledge and reasoning questions. Answer in the language of the user's input (Danish or English).\n\n"
